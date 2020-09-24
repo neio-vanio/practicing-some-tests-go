210f61954918a9b8ab4c3f69b7c8d49280820f2a
Vanio Almeida <vanio@neio.network>
2020-09-23 22:14:35 -0300

    added slice

--- reflection/walks_test.go
+++ reflection/walks_test.go
@@ -1,20 +1,72 @@
 package reflection
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestWalks(t *testing.T) {
-	expected := "Vanio"
-	var result []string
 
-	x := struct {
-		Name string
-	}{expected}
+	cases := []struct {
+		Name     string
+		Input    interface{}
+		Expected []string
+	}{
+		{
+			"Struct with a field string",
+			struct {
+				Name string
+				Nick string
+			}{"Almeida", "Carlos"},
+			[]string{"Almeida", "Carlos"},
+		},
+		{
+			"Struct with a int",
+			struct {
+				Name  string
+				Idade int
+			}{"Vanio", 32},
+			[]string{"Vanio"},
+		},
+		{
+			"Campos aninhados",
+			Person{
+				"Chris",
+				Profile{33, "Londres"},
+			},
+			[]string{"Chris", "Londres"},
+		},
+		{
+			"Campos aninhados",
+			&Person{
+				"Chris",
+				Profile{33, "Londres"},
+			},
+			[]string{"Chris", "Londres"},
+		},
+		{
+			"Slices",
+			[]Profile{
+				{33, "Londres"},
+				{34, "Reykjavík"},
+			},
+			[]string{"Londres", "Reykjavík"},
+		},
+	}
 
-	walks(x, func(arg string) {
-		result = append(result, arg)
-	})
+	for _, test := range cases {
+		t.Run(test.Name, func(t *testing.T) {
 
-	if result[0] != expected {
-		t.Errorf("result '%v', expected '%v'", result[0], expected)
+			var result []string
+
+			walks(test.Input, func(arg string) {
+				result = append(result, arg)
+			})
+
+			if !reflect.DeepEqual(result, test.Expected) {
+				t.Errorf("result '%v', expected '%v'", result, test.Expected)
+			}
+
+		})
 	}
 }
